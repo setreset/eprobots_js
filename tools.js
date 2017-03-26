ba08084a8860560e2d2029f01b8d69f1510f6308
setreset <christian.wehr@gmx.de>
2017-03-26 19:53:43 +0200

    steps max: 100

--- tools.js
+++ tools.js
@@ -27,7 +27,7 @@ function tools_compute(memory) {
     var step_counter = 0;
     var a, b, c;
 
-    while (program_counter >= 0 && (program_counter + 2) < memory.length && step_counter < 200) {
+    while (program_counter >= 0 && (program_counter + 2) < memory.length && step_counter < 100) {
         a = memory[program_counter];
         b = memory[program_counter + 1];
         c = memory[program_counter + 2];
@@ -54,7 +54,7 @@ function tools_compute(memory) {
         }
         step_counter++;
     }
-    if (step_counter>=200){
+    if (step_counter>=100){
         console.log("high stepcounter: " + step_counter);
     }
     return step_counter;
