fa7aaccb01484f747d7b188827d9d13212b16a48
setreset <christian.wehr@gmx.de>
2017-03-26 19:52:05 +0200

    refactoring: baseclass inheritance

--- eprobot.js
+++ eprobot.js
@@ -1,32 +1,44 @@
-var serializationFns = {
-    toJson: function() {
+class Eprobot {
+
+    constructor(s, x_pos, y_pos, init_programm) {
+        // init
+        var t = s.getWorld().getTerrain(x_pos, y_pos);
+        t.setSlotObject(this);
+
+        this.age = 0;
+
+        this.s = s;
+        this.x_pos = x_pos;
+        this.y_pos = y_pos;
+
+        this.init_programm = init_programm;
+        this.working_programm = init_programm.slice(0);
+    }
+
+    toJSON() {
         return {
             id: this.getId(),
-            age: this.getAge(),
-            energy: this.getEnergy(),
+            age: this.age,
+            energy: this.energy,
             x_pos: this.x_pos,
             y_pos: this.y_pos,
-            init_programm: this.getInitialProgram(),
-            working_programm: this.getWorkingProgram()
+            init_programm: this.init_programm,
+            working_programm: this.working_programm
         };
-    },
-    loadState: function(e_state) {
-        this.setAge(e_state.age);
-        this.setEnergy(e_state.energy);
-        this.setWorkingProgramm(e_state.working_programm);
     }
-};
 
-//Object.assign(Herbivore.prototype, serializationFns);
-//Object.assign(Carnivore.prototype, serializationFns);
+    loadState(e_state) {
+        this.age = e_state.age;
+        this.energy = e_state.energy;
+        this.working_programm = e_state.working_programm;
+    }
 
-var inputoutpuFns = {
-    set_input: function() {
+    set_input() {
         var inputval = this.s.getWorld().get_environment_val(this.x_pos,this.y_pos);
         //console.log(inputval);
         var program_length = this.s.getSettings().PROGRAM_LENGTH;
 
-        var working_programm = this.getWorkingProgram();
+        var working_programm = this.working_programm;
         working_programm[program_length-4] = inputval.local_foodcount;
 
         working_programm[program_length-5] = inputval.local_eprobotcount_0;
@@ -43,9 +55,10 @@ var inputoutpuFns = {
         working_programm[program_length-12] = this.getEnergy();
         working_programm[program_length-13] = this.x_pos;
         working_programm[program_length-14] = this.y_pos;
-    },
-    get_control_vals: function() {
-        var working_programm = this.getWorkingProgram();
+    }
+
+    get_control_vals() {
+        var working_programm = this.working_programm;
         var stepcounter = tools_compute(working_programm);
         if (stepcounter>20){
             //var penalty = parseInt((stepcounter-20)/10);
@@ -59,9 +72,42 @@ var inputoutpuFns = {
             working_programm[this.s.getSettings().PROGRAM_LENGTH-3]
         ];
     }
-};
 
-//Object.assign(Herbivore.prototype, inputoutpuFns);
-//Object.assign(Carnivore.prototype, inputoutpuFns);
+    // GETTER / SETTER
 
-// https://javascriptweblog.wordpress.com/2011/05/31/a-fresh-look-at-javascript-mixins/
+    getAge(){
+        return this.age;
+    }
+
+    setAge(new_age){
+        this.age = new_age;
+    }
+
+    incrAge(){
+        this.age++;
+    }
+
+    addEnergy(number) {
+        this.energy += number;
+        if (this.energy < 0){
+            this.energy = 0;
+        }
+    };
+
+    getEnergy(){
+        return this.energy;
+    }
+
+    setEnergy(new_energy){
+        this.energy = new_energy;
+    }
+
+    getPos(){
+        return {"x": this.x_pos, "y": this.y_pos}
+    }
+
+    setPos(new_x_pos, new_y_pos){
+        this.x_pos = new_x_pos;
+        this.y_pos = new_y_pos;
+    }
+}
--- eprobot_carnivore.js
+++ eprobot_carnivore.js
@@ -1,10 +1,19 @@
-function Carnivore(s, x_pos, y_pos, init_programm){
+class Carnivore extends Eprobot{
+    constructor(s, x_pos, y_pos, init_programm) {
+        // init
+        super(s, x_pos, y_pos, init_programm);
+        this.energy = 0;
+    }
 
-    this.newStep = function(){
+    getId(){
+        return OBJECTTYPES.EPROBOT_C;
+    }
+
+    newStep(){
         var forked_ep = null;
 
         // set input
-        if (s.getSettings().SENSE){
+        if (this.s.getSettings().SENSE){
             this.set_input();
         }
 
@@ -32,13 +41,13 @@ function Carnivore(s, x_pos, y_pos, init_programm){
         return forked_ep;
     }
 
-    this.processAction = function(move_action, obstacle_action){
+    processAction(move_action, obstacle_action){
         var forked_ep = null;
 
         if (move_action > 0){
-            var coord__new = s.getWorld().getCoordinates(this, move_action-1);
+            var coord__new = this.s.getWorld().getCoordinates(this, move_action-1);
             if (coord__new){
-                var t_new = s.getWorld().getTerrain(coord__new[0],coord__new[1]);
+                var t_new = this.s.getWorld().getTerrain(coord__new[0],coord__new[1]);
 
                 // ist da auch nichts?
                 if (this.canMoveToField(t_new)){
@@ -46,110 +55,62 @@ function Carnivore(s, x_pos, y_pos, init_programm){
 
                     // position verschieben
                     // alte position loeschen
-                    var t_old = s.getWorld().getTerrain(this.x_pos, this.y_pos);
+                    var t_old = this.s.getWorld().getTerrain(this.x_pos, this.y_pos);
                     t_old.setSlotObject(null);
                     t_new.setSlotObject(this);
-                    t_new.set_trace(this.getId(), s.getSettings().TRACETIME);
+                    t_new.set_trace(this.getId(), this.s.getSettings().TRACETIME);
                     this.setPos(coord__new[0],coord__new[1]);
                 }
             }
         }
 
-        if (obstacle_action > 0 && energy >= s.getSettings().ENERGYCOST_OBSTACLE_C) {
-            var my_t = s.getWorld().getTerrain(this.x_pos,this.y_pos);
-            my_t.setObstacle(s.getSettings().OBSTACLETIME);
-            this.addEnergy(-s.getSettings().ENERGYCOST_OBSTACLE_C);
+        if (obstacle_action > 0 && this.energy >= this.s.getSettings().ENERGYCOST_OBSTACLE_C) {
+            var my_t = this.s.getWorld().getTerrain(this.x_pos,this.y_pos);
+            my_t.setObstacle(this.s.getSettings().OBSTACLETIME);
+            this.addEnergy(-this.s.getSettings().ENERGYCOST_OBSTACLE_C);
         }
 
-        if (energy >= s.getSettings().ENERGYCOST_FORK && age > s.getSettings().CHILDHOOD){
+        if (this.energy >= this.s.getSettings().ENERGYCOST_FORK && this.age > this.s.getSettings().CHILDHOOD){
             if (this.getForkCondition()) {
                 forked_ep = this.fork();
             }
         }
 
         return forked_ep
-    };
-
-    this.set_input = function() {
-        var inputval = this.s.getWorld().get_environment_val(this.x_pos,this.y_pos);
-        //console.log(inputval);
-        var program_length = this.s.getSettings().PROGRAM_LENGTH;
-
-        var working_programm = this.getWorkingProgram();
-        working_programm[program_length-4] = inputval.local_foodcount;
-
-        working_programm[program_length-5] = inputval.local_eprobotcount_0;
-        working_programm[program_length-6] = inputval.local_tracecount_0;
-
-        working_programm[program_length-7] = inputval.local_eprobotcount_1;
-        working_programm[program_length-8] = inputval.local_tracecount_1;
-
-        working_programm[program_length-9] = inputval.local_fossilcount;
-
-        working_programm[program_length-10] = inputval.local_fruitfulness;
-
-        working_programm[program_length-11] = this.getAge();
-        working_programm[program_length-12] = this.getEnergy();
-        working_programm[program_length-13] = this.x_pos;
-        working_programm[program_length-14] = this.y_pos;
-    }
-
-    this.get_control_vals = function() {
-        var working_programm = this.getWorkingProgram();
-        var stepcounter = tools_compute(working_programm);
-        if (stepcounter>20){
-            //var penalty = parseInt((stepcounter-20)/10);
-            var penalty = stepcounter;
-            this.addEnergy(-penalty);
-        }
-
-        return [
-            working_programm[this.s.getSettings().PROGRAM_LENGTH-1],
-            working_programm[this.s.getSettings().PROGRAM_LENGTH-2],
-            working_programm[this.s.getSettings().PROGRAM_LENGTH-3]
-        ];
     }
 
     // HELP-Methods
 
-    this.canMoveToField = function(t_new){
+    canMoveToField(t_new){
         if (t_new.getObstacle()>0){
             return false;
         }
         var obj_on_candidate_field = t_new.getSlotObject();
         return obj_on_candidate_field == null || (obj_on_candidate_field.getId() == OBJECTTYPES.EPROBOT_H);
-    };
+    }
 
-    this.getId = function(){
-        return OBJECTTYPES.EPROBOT_C;
-    };
-
-    this.isAlive = function(){
-        return this.getAge() < this.s.getSettings().LIFETIME_MAX_C;
-    };
-
-    this.preMove = function(t_new){
+    preMove(t_new){
         var obj_on_candidate_field = t_new.getSlotObject();
         if (obj_on_candidate_field != null && obj_on_candidate_field.getId() == OBJECTTYPES.EPROBOT_H) {
             // "eat"
             //if (obj_on_candidate_field.getEnergy()<(this.getEnergy()+50)){
-                obj_on_candidate_field.kill();
-                // neuer eprobot...
-                this.addEnergy(this.s.getSettings().FOOD_ENERGY);
+            obj_on_candidate_field.kill();
+            // neuer eprobot...
+            this.addEnergy(this.s.getSettings().FOOD_ENERGY);
             //}
         }
-    };
+    }
 
-    this.getForkCondition = function(){
+    getForkCondition(){
         return this.s.getEprobots_c().length < this.s.getEprobots_h().length;
-    };
+    }
 
-    this.fork = function(){
+    fork(){
         // freie stelle suchen
         var point = this.s.getWorld().getFreeSpace(this.x_pos, this.y_pos);
         // nachwuchs erzeugen und an freie stelle setzen
         if (point != null){
-            var new_dna = tools_mutate(this.s.getSettingVal("MUTATE_POSSIBILITY"),this.s.getSettingVal("MUTATE_STRENGTH"),this.getInitialProgram());
+            var new_dna = tools_mutate(this.s.getSettingVal("MUTATE_POSSIBILITY"),this.s.getSettingVal("MUTATE_STRENGTH"),this.init_programm);
 
             //console.log("Carnivore fork");
             var forked_ep = new Carnivore(this.s, point.x, point.y, new_dna);
@@ -159,86 +120,14 @@ function Carnivore(s, x_pos, y_pos, init_programm){
         }else{
             return null;
         }
-    };
+    }
 
-    this.doAge = function(){
+    isAlive(){
+        return this.getAge() < this.s.getSettings().LIFETIME_MAX_C;
+    }
+
+    doAge(){
         this.incrAge();
     };
-
-    // GETTER / SETTER
-
-    this.getAge = function(){
-        return age;
-    }
-
-    this.setAge = function(new_age){
-        age = new_age;
-    }
-
-    this.incrAge = function(){
-        age++;
-    }
-
-    this.addEnergy = function (number) {
-        energy += number;
-        if (energy < 0){
-            energy = 0;
-        }
-    };
-
-    this.getEnergy = function(){
-        return energy;
-    }
-
-    this.setEnergy = function(new_energy){
-        energy = new_energy;
-    }
-
-    this.getPos = function(){
-        return {"x": this.x_pos, "y": this.y_pos}
-    }
-
-    this.setPos = function(new_x_pos, new_y_pos){
-        this.x_pos = new_x_pos;
-        this.y_pos = new_y_pos;
-    }
-
-    this.getInitialProgram = function(){
-        return init_programm;
-    }
-
-    this.getWorkingProgram = function(){
-        return working_programm;
-    }
-
-    this.toJSON = function() {
-        return {
-            id: this.getId(),
-            age: age,
-            energy: energy,
-            x_pos: this.x_pos,
-            y_pos: this.y_pos,
-            init_programm: init_programm,
-            working_programm: working_programm
-        };
-    };
-
-    this.loadState = function(e_state) {
-        age = e_state.age;
-        energy = e_state.energy;
-        working_programm = e_state.working_programm;
-    };
-
-    // init
-    var t = s.getWorld().getTerrain(x_pos, y_pos);
-    t.setSlotObject(this);
-
-    var age = 0;
-    var energy = 0;
-
-    this.s = s;
-    this.x_pos = x_pos;
-    this.y_pos = y_pos;
-
-    var working_programm = init_programm.slice(0);
 }
+
